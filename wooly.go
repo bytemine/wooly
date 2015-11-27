24ebd5a5d9f2fa359a8fac5b6a6d6f74195ba638
Ruben Schuller <schuller@bytemine.net>
2015-11-27 16:29:27 +0100

    initial commit

---
+++ wooly.go
@@ -0,0 +1,80 @@
+/* Package wooly wraps Gos default time.Time type to be more fluffy.
+
+It sports a custom UnmarshalJSON implementation which trys to parse multiple time formats before failing.
+Also the Time type is a pointer (in contrast to the default time.Time), so that zero values are not marshalled for omitempty tagged fields
+by the json package.
+*/
+package wooly
+
+import (
+	"time"
+)
+
+
+//"2006-01-02T15:04:05.9999Z", "2006-01-02 15:04:05", "2006-01-02", 
+
+// Layouts is a collection of time formats used in JSON unmarshaling of type Time.
+var Layouts = []string{time.ANSIC, time.UnixDate, time.RubyDate, time.RFC822, time.RFC822Z, time.RFC850, time.RFC1123, time.RFC1123Z, time.RFC3339, time.RFC3339Nano, time.Kitchen, time.Stamp, time.StampMilli, time.StampMicro, time.StampNano}
+
+// parseTime tries to parse a string as multiple time formats (like those of time.Parse).
+// The first match wins. If no format can be parsed successfully, the error for the last
+// tested formatstring is returned.
+func parseTime(layouts []string, value string) (time.Time, error) {
+	var err error
+	for _, v := range layouts {
+		var t time.Time
+		t, err = time.Parse(v, value)
+		if err != nil {
+			continue
+		}
+		return t, nil
+	}
+	return time.Time{}, err
+}
+
+// Time is time.Time with custom json marshaling methods.
+// Each Time object can have its own list of layouts. If it has own layouts these override the
+// package-global layouts.
+type Time struct {
+	time.Time
+	layouts []string
+}
+
+// NewTime returns a new Time value from a time.Time value.
+func New(t time.Time) *Time {
+	return &Time{Time: t}
+}
+
+func (t *Time) Layouts() []string {
+	return t.layouts
+}
+
+func (t *Time) SetLayouts(layouts []string) {
+	t.layouts = layouts
+}
+
+// MarshalJSON implements the json.Marshaler interface.
+// To format the date, the first element of layouts is used (with the objects own layouts overriding the packages).
+func (t *Time) MarshalJSON() ([]byte, error) {
+	var layout string
+	if t.layouts != nil {
+		layout = t.layouts[0]
+	} else {
+		layout = Layouts[0]
+	}
+
+	return []byte(t.Format(`"` + layout + `"`)), nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface.
+// The unmarshaling only fails when parsing has failed for every layout.
+func (t *Time) UnmarshalJSON(data []byte) (err error) {
+	var layouts []string
+	if t.layouts != nil {
+		layouts = t.layouts
+	} else {
+		layouts = Layouts
+	}
+	t.Time, err = parseTime(layouts, string(data[1:len(data)-1]))
+	return
+}
---
+++ wooly_test.go
@@ -0,0 +1,81 @@
+package wooly
+
+import (
+	"time"
+	"testing"
+)
+var testLayouts = []string{time.ANSIC, time.RubyDate}
+
+func TestParseTime(t *testing.T) {
+	x, err := parseTime(testLayouts, "Mon Jan 02 15:04:05 -0700 2006")
+	if err != nil {
+		t.Error(err)
+	}
+
+	if h,m,s := x.Clock(); h != 15 || m != 04 || s != 05 {
+		t.Fail()
+	}
+
+	if y,m,d := x.Date(); y != 2006 || m != time.January || d != 2 {
+		t.Fail()
+	}
+
+	_, err = parseTime(testLayouts, "FailDate")
+	if err == nil {
+		t.Fail()
+	}
+}
+
+func TestMarshalJSON(t *testing.T) {
+	y, err := time.Parse(time.RubyDate, "Mon Jan 02 15:04:05 -0700 2006")
+
+	x := New(y)
+	jx, err := x.MarshalJSON()
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(jx) != `"Mon Jan  2 15:04:05 2006"` {
+		t.Log(string(jx))
+		t.Fail()
+	}
+}
+
+func TestUnmarshalJSON(t *testing.T) {
+	x := new(Time)
+	x.SetLayouts(testLayouts)
+
+	// ANSIC
+	err := x.UnmarshalJSON([]byte(`"Mon Jan  2 15:04:05 2006"`))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if h,m,s := x.Clock(); h != 15 || m != 04 || s != 05 {
+		t.Fail()
+	}
+
+	if y,m,d := x.Date(); y != 2006 || m != time.January || d != 2 {
+		t.Fail()
+	}
+
+	// RubyDate
+	err = x.UnmarshalJSON([]byte(`"Mon Jan 02 15:04:05 -0700 2006"`))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if h,m,s := x.Clock(); h != 15 || m != 04 || s != 05 {
+		t.Fail()
+	}
+
+	if y,m,d := x.Date(); y != 2006 || m != time.January || d != 2 {
+		t.Fail()
+	}
+
+	// Kitchen fails as it is not in x.layouts
+	err = x.UnmarshalJSON([]byte(`"3:04PM"`))
+	if err == nil {
+		t.Fail()
+	}
+}
